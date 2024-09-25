76359a5fd61176518c388487606d462cf077ad4a
Daniel Lasota <slasher.root@gmail.com>
2024-09-25 04:57:40 +0200

    feat: New production_config.json.json, minor changes

--- binance_archiver/main.py
+++ binance_archiver/main.py
@@ -11,11 +11,8 @@ from load_config import load_config
 
 if __name__ == "__main__":
 
-    config = ...
-
-    if is_running_in_docker() is False:
-        load_dotenv('C:/Users/defrg/archer.env')
-        config = load_config('production_config.json')
+    # load_dotenv('C:/Users/defrg/archer.env')
+    # config = load_config('almost_production_config.json')
 
     client = SecretClient(
         vault_url=os.environ.get('VAULT_URL'),
@@ -26,8 +23,7 @@ if __name__ == "__main__":
     config_secret_name = os.environ.get('CONFIG_SECRET_NAME')
     container_name_secret_name = os.environ.get('CONTAINER_NAME_SECRET_NAME')
 
-    if is_running_in_docker() is True:
-        config = json.loads(client.get_secret(config_secret_name).value)
+    config = json.loads(client.get_secret(config_secret_name).value)
     azure_blob_parameters_with_key = client.get_secret(blob_parameters_secret_name).value
     container_name = client.get_secret(container_name_secret_name).value
 
--- binance_archiver/main_listener.py
+++ binance_archiver/main_listener.py
@@ -5,7 +5,7 @@ from load_config import load_config
 
 class ConcreteObserver(Observer):
     def update(self, message):
-        print(f"message: {message}!")
+        print(f"message: {message}")
         ...
 
 if __name__ == '__main__':
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     sample_observer = ConcreteObserver()
 
     data_listener = launch_data_listener(
-        config=load_config('test_config.json'),
+        config=load_config('almost_production_config.json'),
         init_observers=[sample_observer]
     )
