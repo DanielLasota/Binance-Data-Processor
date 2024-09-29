42744c94d6f36241db12eb61dbead3c1d2fd9ae8
Daniel Lasota <slasher.root@gmail.com>
2024-09-29 16:17:00 +0200

    feat: final update of minor things, launching

--- binance_archiver/main_data_sink.py
+++ binance_archiver/main_data_sink.py
@@ -19,20 +19,12 @@ if __name__ == "__main__":
     )
 
     config_secret_name = os.environ.get('CONFIG_SECRET_NAME')
-
-    blob_parameters_secret_name = os.environ.get('AZURE_BLOB_PARAMETERS_WITH_KEY_SECRET_NAME')
-    azure_container_name_secret_name = os.environ.get('AZURE_CONTAINER_NAME_SECRET_NAME')
-
     backblaze_access_key_id_secret_name = os.environ.get('BACKBLAZE_ACCESS_KEY_ID_BINANCE_PROD')
     backblaze_bucket_name_secret_name = os.environ.get('BACKBLAZE_BUCKET_NAME_BINANCE_PROD')
     backblaze_secret_access_key_secret_name = os.environ.get('BACKBLAZE_SECRET_ACCESS_KEY_BINANCE_PROD')
     backblaze_endpoint_url_secret_name = os.environ.get('BACKBLAZE_ENDPOINT_URL_BINANCE_PROD')
 
     config = json.loads(client.get_secret(config_secret_name).value)
-
-    azure_blob_parameters_with_key = client.get_secret(blob_parameters_secret_name).value
-    azure_container_name = client.get_secret(azure_container_name_secret_name).value
-
     backblaze_access_key_id = client.get_secret(backblaze_access_key_id_secret_name).value
     backblaze_secret_access_key = client.get_secret(backblaze_secret_access_key_secret_name).value
     backblaze_endpoint_url = client.get_secret(backblaze_endpoint_url_secret_name).value
