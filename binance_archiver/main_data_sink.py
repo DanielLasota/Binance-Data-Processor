5be8e94c6ca6c1f83475a7b88bcb7041ba4435b9
Daniel Lasota <grossmann.root@gmail.com>
2025-01-14 18:29:40 +0100

    fix: improvements

--- binance_archiver/main_data_sink.py
+++ binance_archiver/main_data_sink.py
@@ -21,10 +21,10 @@ if __name__ == "__main__":
     )
 
     config_secret_name = os.environ.get('CONFIG_SECRET_NAME')
-    backblaze_access_key_id_secret_name = os.environ.get('BACKBLAZE_ACCESS_KEY_ID_BINANCE_TEST')
-    backblaze_bucket_name_secret_name = os.environ.get('BACKBLAZE_BUCKET_NAME_BINANCE_TEST')
-    backblaze_secret_access_key_secret_name = os.environ.get('BACKBLAZE_SECRET_ACCESS_KEY_BINANCE_TEST')
-    backblaze_endpoint_url_secret_name = os.environ.get('BACKBLAZE_ENDPOINT_URL_BINANCE_TEST')
+    backblaze_access_key_id_secret_name = os.environ.get('BACKBLAZE_ACCESS_KEY_ID_BINANCE_PROD')
+    backblaze_bucket_name_secret_name = os.environ.get('BACKBLAZE_BUCKET_NAME_BINANCE_PROD')
+    backblaze_secret_access_key_secret_name = os.environ.get('BACKBLAZE_SECRET_ACCESS_KEY_BINANCE_PROD')
+    backblaze_endpoint_url_secret_name = os.environ.get('BACKBLAZE_ENDPOINT_URL_BINANCE_PROD')
 
     config = json.loads(client.get_secret(config_secret_name).value)
     backblaze_access_key_id = client.get_secret(backblaze_access_key_id_secret_name).value
