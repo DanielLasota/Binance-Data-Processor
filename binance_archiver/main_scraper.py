aa3f86244e8ea0057466b848e2d9d7efb0c17bf1
Daniel Lasota <grossmann.root@gmail.com>
2025-01-17 10:24:32 +0100

    second round, first vm

--- binance_archiver/main_scraper.py
+++ binance_archiver/main_scraper.py
@@ -17,10 +17,10 @@ if __name__ == '__main__':
         credential=DefaultAzureCredential()
     )
 
-    backblaze_access_key_id_secret_name = os.environ.get('BACKBLAZE_ACCESS_KEY_ID_BINANCE_PROD')
-    backblaze_bucket_name_secret_name = os.environ.get('BACKBLAZE_BUCKET_NAME_BINANCE_PROD')
-    backblaze_secret_access_key_secret_name = os.environ.get('BACKBLAZE_SECRET_ACCESS_KEY_BINANCE_PROD')
-    backblaze_endpoint_url_secret_name = os.environ.get('BACKBLAZE_ENDPOINT_URL_BINANCE_PROD')
+    backblaze_access_key_id_secret_name = os.environ.get('BACKBLAZE_ACCESS_KEY_ID_BINANCE_TEST')
+    backblaze_bucket_name_secret_name = os.environ.get('BACKBLAZE_BUCKET_NAME_BINANCE_TEST')
+    backblaze_secret_access_key_secret_name = os.environ.get('BACKBLAZE_SECRET_ACCESS_KEY_BINANCE_TEST')
+    backblaze_endpoint_url_secret_name = os.environ.get('BACKBLAZE_ENDPOINT_URL_BINANCE_TEST')
 
     backblaze_access_key_id = client.get_secret(backblaze_access_key_id_secret_name).value
     backblaze_secret_access_key = client.get_secret(backblaze_secret_access_key_secret_name).value
@@ -29,8 +29,8 @@ if __name__ == '__main__':
 
     download_csv_data(
         # dump_path='C:/Users/defrg/binance_data_main/',
-        start_date='05-01-2025',
-        end_date='05-01-2025',
+        start_date='15-01-2025',
+        end_date='15-01-2025',
         backblaze_access_key_id=backblaze_access_key_id,
         backblaze_secret_access_key=backblaze_secret_access_key,
         backblaze_endpoint_url=backblaze_endpoint_url,
