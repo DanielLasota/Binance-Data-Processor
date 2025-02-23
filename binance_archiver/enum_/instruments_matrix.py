a28a505f2693782691a0f448c03a611ac116119d
Daniel Lasota <slasher.root@gmail.com>
2025-02-23 13:13:35 +0100

    fix(data_sink_config-instruments_matrix): fixed bug demanding 3 markets
    fix(command_line_interface): fixed adding pair that already exists

--- binance_archiver/enum_/instruments_matrix.py
+++ binance_archiver/enum_/instruments_matrix.py
@@ -1,60 +1,45 @@
-import logging
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from binance_archiver.enum_.market_enum import Market
 import pprint
 
-
 @dataclass
 class InstrumentsMatrix:
-    spot: list[str] = field(default_factory=list)
-    usd_m_futures: list[str] = field(default_factory=list)
-    coin_m_futures: list[str] = field(default_factory=list)
+    spot: list[str] | None = None
+    usd_m_futures: list[str] | None = None
+    coin_m_futures: list[str] | None = None
 
     def __post_init__(self):
-        self.dict: dict[Market, list[str]] = {
-            Market.SPOT: self.spot,
-            Market.USD_M_FUTURES: self.usd_m_futures,
-            Market.COIN_M_FUTURES: self.coin_m_futures
-        }
+        self.dict: dict[Market, list[str]] = {}
+        if self.spot is not None and self.spot:
+            self.dict[Market.SPOT] = self.spot
+        if self.usd_m_futures is not None and self.usd_m_futures:
+            self.dict[Market.USD_M_FUTURES] = self.usd_m_futures
+        if self.coin_m_futures is not None and self.coin_m_futures:
+            self.dict[Market.COIN_M_FUTURES] = self.coin_m_futures
 
     def __str__(self) -> str:
         return pprint.pformat(self.dict, indent=1)
 
-    def add_pair(
-            self,
-            market: Market,
-            pair: str
-    ) -> None:
-        logger = logging.getLogger('binance_archiver')
+    def add_pair(self, market: Market, pair: str) -> None:
         if market not in self.dict:
             self.dict[market] = []
         if pair not in self.dict[market]:
             self.dict[market].append(pair)
-            logger.info(f"Added instrument '{pair}' to market '{market.name}'.")
         else:
-            logger.info(f"Instrument '{pair}' already exists in market '{market.name}'.")
+            raise Exception(f"Instrument '{pair}' already exists in market '{market.name}'")
 
-    def remove_pair(
-            self,
-            market: Market,
-            instrument: str
-    ) -> None:
-        logger = logging.getLogger('binance_archiver')
-
+    def remove_pair(self, market: Market, instrument: str) -> None:
         if market in self.dict:
-            try:
+            if instrument in self.dict[market]:
                 self.dict[market].remove(instrument)
-                logger.info(f"Deleted instrument '{instrument}' from market '{market.name}'.")
-            except ValueError:
-                logger.info(f"Instrument '{instrument}' not found in market '{market.name}'.")
+            else:
+                raise Exception(f'There is no instrument {instrument} subscribed in market {market}')
         else:
-            logger.info(f"Market '{market.name}' does not exist.")
-
+            raise Exception(f"Market '{market.name}' does not exist.")
 
     def get_pairs(self, market: Market) -> list[str]:
         return self.dict.get(market, [])
 
     def is_pair(self, market: Market, instrument: str) -> bool:
         instruments = self.dict.get(market, [])
-        exists = instrument in instruments
-        return exists
+        return instrument in instruments
