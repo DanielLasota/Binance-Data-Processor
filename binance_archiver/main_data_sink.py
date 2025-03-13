4a10943e97f8015e28c3e4505154b7d093781bea
Daniel Lasota <slasher.root@gmail.com>
2025-03-13 22:52:25 +0100

    feat(scraper)!: add diff and trade scraper with individual_column_checker.py

--- binance_archiver/main_data_sink.py
+++ binance_archiver/main_data_sink.py
@@ -2,12 +2,13 @@ import time
 from dotenv import load_dotenv
 
 from binance_archiver import load_config_from_json, DataSinkConfig, launch_data_sink
+from binance_archiver.enum_.storage_connection_parameters import StorageConnectionParameters
 
 
 if __name__ == "__main__":
 
     load_dotenv('binance-archiver-2.env')
-    config_from_json = load_config_from_json(json_filename='production_config.json')
+    config_from_json = load_config_from_json(json_filename='almost_production_config.json')
 
     data_sink_config = DataSinkConfig(
         instruments={
@@ -20,7 +21,8 @@ if __name__ == "__main__":
             "snapshot_fetcher_interval_seconds": config_from_json["snapshot_fetcher_interval_seconds"],
             "websocket_life_time_seconds": config_from_json["websocket_life_time_seconds"]
         },
-        data_save_target=config_from_json['data_save_target']
+        data_save_target=config_from_json['data_save_target'],
+        storage_connection_parameters=StorageConnectionParameters()
     )
 
     data_sink = launch_data_sink(data_sink_config=data_sink_config)
