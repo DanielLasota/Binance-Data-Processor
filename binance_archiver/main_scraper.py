0d01a3dc517491e2e36b66c28a5f95fbd8119143
Daniel Lasota <slasher.root@gmail.com>
2025-03-15 22:23:06 +0100

    feat(scraper): Add skip_existing parameter. data_quality_checker.py refactor

--- binance_archiver/main_scraper.py
+++ binance_archiver/main_scraper.py
@@ -9,14 +9,20 @@ from binance_archiver.scraper import download_csv_data
 if __name__ == '__main__':
     load_dotenv('binance-archiver-1.env')
 
-    storage_connection_parameters = StorageConnectionParameters()
-
     download_csv_data(
-        date_range=['06-03-2025', '07-03-2025'],
-        storage_connection_parameters=storage_connection_parameters,
+        date_range=['10-03-2025', '10-03-2025'],
+        storage_connection_parameters=StorageConnectionParameters(),
         pairs=['TRXUSDT'],
-        markets=['USD_M_FUTURES', 'COIN_M_FUTURES'],
-        stream_types=['TRADE_STREAM', 'DIFFERENCE_DEPTH_STREAM']
+        markets=[
+            'SPOT',
+            'USD_M_FUTURES',
+            'COIN_M_FUTURES'
+        ],
+        stream_types=[
+            'TRADE_STREAM',
+            'DIFFERENCE_DEPTH_STREAM'
+        ],
+        skip_existing=True
     )
 
     conduct_data_quality_analysis_on_specified_csv_list(
