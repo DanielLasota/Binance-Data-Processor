dcc92c4df656ba29dee26067ad69ddca7fc32eb9
Daniel Lasota <slasher.root@gmail.com>
2025-03-16 11:32:25 +0100

    feat(scraper): Add amount_of_files_to_be_downloaded_at_once parameter

--- binance_archiver/enum_/stream_type_enum.py
+++ binance_archiver/enum_/stream_type_enum.py
@@ -2,7 +2,7 @@ from enum import Enum, auto
 
 
 class StreamType(Enum):
-    DIFFERENCE_DEPTH_STREAM = auto()
-    TRADE_STREAM = auto()
-    DEPTH_SNAPSHOT = auto()
+    DIFFERENCE_DEPTH_STREAM = 'difference_depth_stream'
+    TRADE_STREAM = 'trade_stream'
+    DEPTH_SNAPSHOT = 'depth_snapshot'
     ...
