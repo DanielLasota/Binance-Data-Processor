242141ede601f653bebf36270ea46263a5524c5c
Daniel Lasota <slasher.root@gmail.com>
2025-06-14 14:55:28 +0200

    refactor: convert concatenator to int enums for cppp

--- binance_data_processor/enums/market_enum.py
+++ binance_data_processor/enums/market_enum.py
@@ -2,7 +2,15 @@ from enum import Enum
 
 
 class Market(Enum):
+    UNKNOWN = 'unknown'
     SPOT = 'spot'
     USD_M_FUTURES = 'usd_m_futures'
     COIN_M_FUTURES = 'coin_m_futures'
     ...
+
+M_STREAM_CODE = {
+    Market.UNKNOWN:         0,
+    Market.SPOT:            1,
+    Market.USD_M_FUTURES:   2,
+    Market.COIN_M_FUTURES:  3,
+}
